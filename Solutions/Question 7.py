871db72ffeb5a1b2ebae16c55148c707c5e8a0a3
kaustubh43 <68014091+kaustubh43@users.noreply.github.com>
2023-02-16 12:32:06 +0530

    Question 7

---
+++ Solutions/Question 7.py
@@ -0,0 +1,26 @@
+## Question 7
+## Level 2
+##
+## Question: Write a program which takes 2 digits,
+## X,Y as input and generates a 2-dimensional array.
+## The element value in the i-th row and j-th column of the array
+## should be i*j. Note: i=0,1.., X-1; j=0,1,¡­Y-1. Example Suppose the
+## following inputs are given to the program: 3,5 Then, the output of the
+## program should be: [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
+##
+## Hints: Note: In case of input data being supplied to
+## the question, it should be assumed to be a console input in a comma-separated form.
+
+val = input('Enter the value')
+val = val.split(',')
+x_dim = int(val[0])
+y_dim = int(val[1])
+
+multilist = [[0 for col in range(y_dim)] for row in range(x_dim)]
+
+for i in range(x_dim):
+    for j in range(y_dim):
+        multilist[i][j] = (i+1) * (j+1)
+
+print(multilist)
+        
