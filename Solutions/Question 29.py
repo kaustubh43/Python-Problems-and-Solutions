70c95863b7da2b8411552d741a862fbc204b0869
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-02-21 15:24:23 +0530

    Q29 and 30

---
+++ Solutions/Question 29.py
@@ -0,0 +1,14 @@
+## Question 29
+## Define a function that can receive two integral numbers in
+## string form and compute their sum and then print it in console.
+##
+## Hints:
+##
+## Use int() to convert a string to integer.
+##
+## Solution
+
+def summation(s1, s2):
+    print(int(s1)+int(s2))
+
+summation(3,5)
---
+++ Solutions/Question 30.py
@@ -0,0 +1,16 @@
+## Question 30
+## Define a function that can accept two strings as input
+## and concatenate them and then print it in console.
+##
+## Hints:
+##
+## Use + to concatenate the strings
+##
+## Solution
+
+
+def conca(s1,s2):
+    print(s1+s2)
+
+
+conca('Hello', 'My Friend')
