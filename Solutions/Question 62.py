e80c8629efef6ec74591fe869827835f7cc9404e
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-02-25 23:42:45 +0530

    EOD25FEB

---
+++ Solutions/Question 62.py
@@ -0,0 +1,11 @@
+# Question 62
+# Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+#
+# Hints:
+#
+# Use unicode() function to convert.
+#
+# Solution:
+
+s = input('Enter a string')
+print('Hello'.encode('utf-8'))
