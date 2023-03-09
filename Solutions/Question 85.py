91eb343034b586b6b524279a9845a36648286574
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-03-09 20:35:37 +0530

    EOD09MAR

---
+++ Solutions/Question 85.py
@@ -0,0 +1,11 @@
+# Please write a program to shuffle and print the list [3,6,7,8].
+#
+# Hints: Use shuffle() function to shuffle a list.
+#
+# Solution:
+
+from random import shuffle
+
+li = [3, 4, 5, 6, 8]
+shuffle(li)
+print(li)
---
+++ Solutions/Question 86.py
@@ -0,0 +1,17 @@
+# Question 86
+# Please write a program to generate all sentences
+# where subject is in ["I", "You"] and verb is in ["Play", "Love"]
+# and the object is in ["Hockey","Football"].
+#
+# Hints: Use list[index] notation to get a element from a list.
+#
+# Solution:
+
+x = ['I', 'You']
+y = ['Play', 'Love']
+z = ['Hockey', 'Football']
+
+for i in x:
+    for j in y:
+        for k in z:
+            print(i, j, k)
